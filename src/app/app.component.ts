869ec1b4fba43593cb15f759deecf1ce5ec76621
dpk <deep.7178@gmail.com>
2018-09-17 00:25:37 +0530

    add first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-app';
+  clickMessage = '';
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+  }
 }
